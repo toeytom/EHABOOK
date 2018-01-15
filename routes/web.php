349d5491cd7655faf3e7bf002727e42abafb41b0
Worawut Sangangam <58160090@go.buu.ac.th>
2018-01-15 19:27:25 +0700

    add profile

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,10 @@ Route::get('auth/google/callback', 'SocialAuthFacebookController@callbackg');
 Route::get('auth/twitter', 'SocialAuthFacebookController@redirectt');
 Route::get('auth/twitter/callback', 'SocialAuthFacebookController@callbackt');
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
 
 Route::get('/profile','ProfileController@profile');
 Route::post('/addProfile','ProfileController@addProfile');
-=======
+
 Route::get('/changepass','ChangePasswordController@index')->name('changepass');
 Route::post('/changepass','ChangePasswordController@change')->name('changepass');
->>>>>>> ee99846d39093ac91833283dc938bb9f15731081
+
