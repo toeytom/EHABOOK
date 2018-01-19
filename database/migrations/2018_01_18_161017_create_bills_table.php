6dd2a7f038ae9d3fa62c5eaacfc0e58690f5b9f5
Worawut Sangangam <58160090@go.buu.ac.th>
2018-01-19 15:16:10 +0700

    fix migrate

--- database/migrations/2018_01_18_161017_create_bills_table.php
+++ database/migrations/2018_01_18_161017_create_bills_table.php
@@ -12,23 +12,7 @@ class CreateBillsTable extends Migration
      * @return void
      * 
      */
-    Schema::table('users', function ($table) {
-        $table->string('stripe_id')->nullable();
-        $table->string('card_brand')->nullable();
-        $table->string('card_last_four')->nullable();
-        $table->timestamp('trial_ends_at')->nullable();
-    });
-    Schema::create('cards', function ($table) {
-        $table->increments('id');
-        $table->integer('user_id');
-        $table->string('name');
-        $table->string('stripe_id');
-        $table->string('stripe_plan');
-        $table->integer('quantity');
-        $table->timestamp('trial_ends_at')->nullable();
-        $table->timestamp('ends_at')->nullable();
-        $table->timestamps();
-    });
+
     public function up()
     {
         Schema::table('users', function ($table) {
