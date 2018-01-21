76585b653999a002642ad91ba4cfaaa7b40face5
Worawut Sangangam <58160090@go.buu.ac.th>
2018-01-21 19:03:12 +0700

    input book and comments

---
+++ database/migrations/2018_01_20_150534_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('comment_id');
+            $table->string('comment_taxt');
+            $table->dateTime('date_of_comment');
+            $table->timestamps();
+            $table->foreign('user_user_id')->references('user_id')->on('users');
+            $table->foreign('book_book_id')->references('book_id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
