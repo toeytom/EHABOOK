eede6ec7f6939c6ac9aa779258bf25daf0cf7fd0
toeytom <toeytom2551@hotmail.com>
2018-01-23 21:41:46 +0700

    update

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-end">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
